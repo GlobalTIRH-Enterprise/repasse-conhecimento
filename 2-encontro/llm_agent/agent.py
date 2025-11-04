f8b301aa299f412179a48e12e4b311e722ec401b
Merus23 <mateus.satira@gmail.com>
2025-11-03 23:17:58 -0300

    repasse segundo encontro

---
+++ 2-encontro/llm_agent/agent.py
@@ -0,0 +1,17 @@
+from google.adk.agents.llm_agent import Agent
+from datetime import datetime
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_current_time():    
+    return datetime.now().isoformat()   
+
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    tools=[get_current_time],
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge. if the user asks about the time use the get_current_time tool to get the current time.',
+)
